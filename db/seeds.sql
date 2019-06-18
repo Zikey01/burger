cfb9339b008dd3e2f02f78023ad6f6a92daf193b
Fred Leinweber <fred.leinweber@gmail.com>
2019-06-17 22:11:14 -0500

    commit file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawiian', true);
+INSERT INTO burgers (burger_name) VALUES ('Double');
